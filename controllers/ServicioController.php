0dc8a2a4a27c940cd7b2d54653b31a4d3e8791b7
RicardoPayan <a218206581@unison.mx>
2022-07-14 13:17:28 -0700

    Proyecto terminado

--- controllers/ServicioController.php
+++ controllers/ServicioController.php
@@ -11,8 +11,13 @@ class ServicioController{
             session_start();
         }
 
+        isAdmin();
+
+        $servicios = Servicio::all();
+
         $router->render('/servicios/index',[
-            'nombre'=>$_SESSION['nombre']
+            'nombre'=>$_SESSION['nombre'],
+            'servicios'=>$servicios
         ]);
     }
 
@@ -20,6 +25,8 @@ class ServicioController{
         if(!isset($_SESSION)){
             session_start();
         }
+
+        isAdmin();
 
         $servicio = new Servicio();
         $alertas=[];
@@ -48,18 +55,43 @@ class ServicioController{
             session_start();
         }
 
-        if($_SERVER['REQUEST_METHOD']==='POST'){
+        isAdmin();
 
+        if(!is_numeric($_GET['id'])) return;
+
+        $servicio = Servicio::find($_GET['id']);
+        $alertas=[];
+
+        if($_SERVER['REQUEST_METHOD']==='POST'){
+            $servicio->sincronizar($_POST);
+
+            $alertas = $servicio->validar();
+
+            if(empty($alertas)){
+                $servicio->guardar();
+                header('Location:/servicios');
+            }
         }
 
         $router->render('/servicios/actualizar',[
-            'nombre'=>$_SESSION['nombre']
+            'nombre'=>$_SESSION['nombre'],
+            'servicio'=>$servicio,
+            'alertas'=>$alertas
         ]);
     }
 
-    public static function eliminar(Router $router){
-        if($_SERVER['REQUEST_METHOD']==='POST'){
+    public static function eliminar(){
+        if(!isset($_SESSION)){
+            session_start();
+        }
 
+        isAdmin();
+
+        if($_SERVER['REQUEST_METHOD']==='POST'){
+            $id = $_POST['id'];
+            $servicio = Servicio::find($id);
+            $servicio->eliminar();
+            header('Location:/servicios');
         }
     }
 }
